10b7f43721014836e9c8a0f610f9563685d221b8
Mattias Appelgren <mattias@ppelgren.se>
2024-09-14 16:19:13 +0200

    Fix shutdown

--- cmd/vanity-ssh-keygen/main.go
+++ cmd/vanity-ssh-keygen/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log/slog"
 	"os"
@@ -68,6 +69,163 @@ type cli struct {
 	StatsLogInterval time.Duration    `help:"Statistics will be printed at this interval, set to 0 to disable" default:"2s"`
 }
 
+func main() {
+	ml := matcher.MatcherList()
+	kl := keygen.KeygenList()
+
+	c := cli{}
+	kongctx := kong.Parse(&c, kong.Vars{
+		"version":         versionString(),
+		"default_threads": fmt.Sprintf("%d", runtime.NumCPU()),
+		"keytypes":        strings.Join(kl.Names(), ","),
+		"default_keytype": kl.Names()[0],
+		"matchers":        strings.Join(ml.Names(), ","),
+		"default_matcher": ml.Names()[0],
+	})
+
+	ctx, stop := signal.NotifyContext(context.Background(),
+		os.Interrupt,
+		os.Kill,
+		syscall.SIGTERM,
+	)
+
+	shutdownFuncs := []func(context.Context) error{}
+
+	if c.Metrics {
+		exporter, err := otlpmetrichttp.New(ctx)
+		if err != nil {
+			slog.Error("Could not create metric exporter", "error", err)
+			os.Exit(1)
+		}
+		resource, err := resource.Merge(resource.Default(),
+			resource.NewWithAttributes(semconv.SchemaURL,
+				semconv.ServiceName(serviceName),
+				semconv.ServiceVersion(version),
+				semconv.ServiceInstanceID(instanceID),
+			))
+		if err != nil {
+			slog.Error("Could not crete metric resources", "error", err)
+			os.Exit(1)
+		}
+		provider := metric.NewMeterProvider(
+			metric.WithReader(metric.NewPeriodicReader(exporter)),
+			metric.WithResource(resource),
+		)
+
+		shutdownFuncs = append(shutdownFuncs, func(ctx context.Context) error {
+			slog.Info("Shutting down metric provider")
+			if err := provider.Shutdown(ctx); err != nil {
+				return fmt.Errorf("failed to stop metric provider: %w", err)
+			}
+			return nil
+		})
+
+		otel.SetMeterProvider(provider)
+	}
+
+	if c.OtelLogs {
+		exporter, err := otlploghttp.New(ctx)
+		if err != nil {
+			panic(err)
+		}
+
+		provider := log.NewLoggerProvider(
+			log.WithProcessor(
+				log.NewBatchProcessor(exporter),
+			))
+
+		shutdownFuncs = append(shutdownFuncs, func(ctx context.Context) error {
+			slog.Info("Shutting down logger provider")
+			if err := provider.Shutdown(ctx); err != nil {
+				return fmt.Errorf("failed to stop logger provider: %w", err)
+			}
+			return nil
+		})
+
+		global.SetLoggerProvider(provider)
+
+		slog.SetDefault(otelslog.NewLogger(serviceName,
+			otelslog.WithSchemaURL(semconv.SchemaURL),
+			otelslog.WithVersion(version),
+		))
+
+	}
+
+	if c.Profile {
+		f, err := os.Create("./pprof")
+		if err != nil {
+			slog.Error("Could not create profile file", "error", err)
+			os.Exit(1)
+		}
+		if err := pprof.StartCPUProfile(f); err != nil {
+			slog.Error("Could not start profiling", "error", err)
+			os.Exit(1)
+		}
+
+		shutdownFuncs = append(shutdownFuncs, func(_ context.Context) error {
+			slog.Info("Stopping CPU profile")
+			pprof.StopCPUProfile()
+			return nil
+		})
+
+	}
+
+	if c.PyroscopeProfile {
+		profiler, err := pyroscope.Start(pyroscope.Config{
+			Logger:          pyroscope.StandardLogger,
+			ApplicationName: serviceName,
+			// ServerAddress:     os.Getenv("PYROSCOPE_SERVER"),
+			BasicAuthUser:     os.Getenv("GRAFANA_INSTANCE_ID"),
+			BasicAuthPassword: os.Getenv("TOKEN"),
+			Tags: map[string]string{
+				"instanceID": instanceID,
+				"platform":   fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH),
+				"cpus":       fmt.Sprintf("%d", runtime.NumCPU()),
+			},
+		})
+		if err != nil {
+			slog.Error("Could not start profiling", "error", err)
+			os.Exit(1)
+		}
+
+		shutdownFuncs = append(shutdownFuncs, func(_ context.Context) error {
+			slog.Info("Stopping profiler")
+			return profiler.Stop()
+		})
+	}
+
+	go func() {
+		<-ctx.Done()
+		slog.Info("Shutting down", "funcs", len(shutdownFuncs))
+
+		// Call shutdown functions with a new context with timeout.
+		shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		var err error
+		for _, fn := range shutdownFuncs {
+			err = errors.Join(err, fn(shutdownCtx))
+		}
+		fmt.Printf("err: %v\n", err)
+		kongctx.Exit(1)
+	}()
+
+	m, err := ml.Get(c.Matcher)
+	if err != nil {
+		slog.Error("Invalid matcher", "error", err)
+		os.Exit(1)
+	}
+	m.SetMatchString(c.MatchString)
+	k, ok := kl.Get(c.KeyType)
+	if !ok {
+		slog.Error("Invalid key type", "error", err)
+		os.Exit(1)
+	}
+	runKeygen(c, m, k)
+
+	stop()
+	kongctx.Exit(0)
+}
+
 func runKeygen(c cli, matcher matcher.Matcher, kg keygen.Keygen) {
 	var workers []workerpool.Worker[chan keygen.SSHKey]
 	for i := 0; i < c.Threads; i++ {
@@ -97,143 +255,6 @@ func runKeygen(c cli, matcher matcher.Matcher, kg keygen.Keygen) {
 	wps := wp.GetStats()
 	printStats(wps)
 	outputKey(c, wps.Elapsed, result)
-}
-
-func main() {
-	ml := matcher.MatcherList()
-	kl := keygen.KeygenList()
-
-	commit := "none"
-	date := "unknown"
-	if info, ok := debug.ReadBuildInfo(); ok {
-		for _, kv := range info.Settings {
-			if kv.Key == "vcs.revision" {
-				commit = kv.Value
-			} else if kv.Key == "vcs.time" {
-				date = kv.Value
-			}
-		}
-	}
-
-	c := cli{}
-	kongctx := kong.Parse(&c, kong.Vars{
-		"version":         fmt.Sprintf("%s commit:%s date:%s goVersion:%s platform:%s/%s", version, commit, date, runtime.Version(), runtime.GOOS, runtime.GOARCH),
-		"default_threads": fmt.Sprintf("%d", runtime.NumCPU()),
-		"keytypes":        strings.Join(kl.Names(), ","),
-		"default_keytype": kl.Names()[0],
-		"matchers":        strings.Join(ml.Names(), ","),
-		"default_matcher": ml.Names()[0],
-	})
-
-	ctx := context.Background()
-	if c.Metrics {
-		exporter, err := otlpmetrichttp.New(ctx)
-		if err != nil {
-			slog.Error("Could not create metric exporter", "error", err)
-			os.Exit(1)
-		}
-		resource, err := resource.Merge(resource.Default(),
-			resource.NewWithAttributes(semconv.SchemaURL,
-				semconv.ServiceName(serviceName),
-				semconv.ServiceVersion(version),
-				semconv.ServiceInstanceID(instanceID),
-			))
-		if err != nil {
-			slog.Error("Could not crete metric resources", "error", err)
-			os.Exit(1)
-		}
-		provider := metric.NewMeterProvider(
-			metric.WithReader(metric.NewPeriodicReader(exporter)),
-			metric.WithResource(resource),
-		)
-
-		otel.SetMeterProvider(provider)
-
-	}
-	if c.OtelLogs {
-		logExporter, err := otlploghttp.New(ctx)
-		if err != nil {
-			panic(err)
-		}
-
-		loggerProvider := log.NewLoggerProvider(
-			log.WithProcessor(
-				log.NewBatchProcessor(logExporter),
-			))
-		//shutdownFuncs = append(shutdownFuncs, loggerProvider.Shutdown)
-		global.SetLoggerProvider(loggerProvider)
-		slog.SetDefault(otelslog.NewLogger(serviceName,
-			otelslog.WithSchemaURL(semconv.SchemaURL),
-			otelslog.WithVersion(version),
-		))
-	}
-
-	if c.Profile {
-		f, err := os.Create("./pprof")
-		if err != nil {
-			slog.Error("Could not create profile file", "error", err)
-			os.Exit(1)
-		}
-		if err := pprof.StartCPUProfile(f); err != nil {
-			slog.Error("Could not start profiling", "error", err)
-			os.Exit(1)
-		}
-		defer pprof.StopCPUProfile()
-		// listening OS shutdown singal
-		signalChan := make(chan os.Signal, 1)
-		signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
-		go func() {
-			<-signalChan
-			slog.Info("Got shutdown signal.")
-			pprof.StopCPUProfile()
-			os.Exit(1)
-		}()
-	}
-	if c.PyroscopeProfile {
-		profiler, err := pyroscope.Start(pyroscope.Config{
-			Logger:          pyroscope.StandardLogger,
-			ApplicationName: serviceName,
-			// ServerAddress:     os.Getenv("PYROSCOPE_SERVER"),
-			BasicAuthUser:     os.Getenv("GRAFANA_INSTANCE_ID"),
-			BasicAuthPassword: os.Getenv("TOKEN"),
-			Tags: map[string]string{
-				"instanceID": instanceID,
-				"platform":   fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH),
-				"cpus":       fmt.Sprintf("%d", runtime.NumCPU()),
-			},
-		})
-		if err != nil {
-			slog.Error("Could not start profiling", "error", err)
-			os.Exit(1)
-		}
-		defer func() {
-			_ = profiler.Stop()
-		}()
-		// listening OS shutdown singal
-		signalChan := make(chan os.Signal, 1)
-		signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
-		go func() {
-			<-signalChan
-			slog.Info("Got shutdown signal.")
-			_ = profiler.Stop()
-			os.Exit(1)
-		}()
-	}
-
-	m, err := ml.Get(c.Matcher)
-	if err != nil {
-		slog.Error("Invalid matcher", "error", err)
-		os.Exit(1)
-	}
-	m.SetMatchString(c.MatchString)
-
-	k, ok := kl.Get(c.KeyType)
-	if !ok {
-		slog.Error("Invalid key type", "error", err)
-		os.Exit(1)
-	}
-	runKeygen(c, m, k)
-	kongctx.Exit(0)
 }
 
 func printStats(wps *workerpool.WorkerPoolStats) {
@@ -269,4 +290,28 @@ func outputKey(c cli, elapsed time.Duration, result keygen.SSHKey) {
 		_ = os.WriteFile(jsonFileName, file, 0o600)
 		slog.Info("Result keypair stored", "json_file", jsonFileName)
 	}
+}
+
+func versionString() string {
+	commit := "none"
+	date := "unknown"
+	if info, ok := debug.ReadBuildInfo(); ok {
+		for _, kv := range info.Settings {
+			switch kv.Key {
+			case "vcs.revision":
+				commit = kv.Value
+			case "vcs.time":
+				date = kv.Value
+			}
+		}
+	}
+
+	return fmt.Sprintf("%s commit:%s date:%s goVersion:%s platform:%s/%s",
+		version,
+		commit,
+		date,
+		runtime.Version(),
+		runtime.GOOS,
+		runtime.GOARCH,
+	)
 }
